c5c79514374d8be3ef3990b5d1d30d388ab6bdc0
yokuba <aflyingfemme@gmail.com>
2018-07-30 22:08:28 -0700

    Add association tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 	factory :user do
-		sequence(:name) { |n| "test#{n}" }
-		sequence(:email) { |n| "test#{n}@test.com" }
+		name "test" 
+		email "test@test.com" 
 		password 'password'
 		password_confirmation 'password'
 	end
---
+++ spec/factories/values.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+		factory :value do 
+			title "ethical"
+			description "low ratio salary of ceo to employees"
+			user
+			company
+	end
+end
