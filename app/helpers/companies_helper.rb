2fc09d904c81f8828c0536b73ec6c9d50c560af4
yokuba <aflyingfemme@gmail.com>
2018-08-26 20:19:10 -0700

    Add company details

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,2 +1,21 @@
 module CompaniesHelper
+	def add_new_company_partial_path
+    if user_signed_in?
+      'companies/category/add_new_company/signed_in'
+    else
+      'companies/category/add_new_company/not_signed_in'
+    end
+  end
+
+  def all_values_button_partial_path
+    if params[:value].blank?
+      'companies/category/values/all_selected'
+    else
+      'companies/category/values/all_not_selected'
+    end
+  end
+
+  def no_companies_partial_path
+  	@companies.empty? ? 'companies/category/no_companies' : 'shared/empty_partial'
+	end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,24 @@
 class Company < ApplicationRecord
+	include DumbStockClient
+
 	has_many :values
 	has_many :users, :through => :values
+
+	def self.create_companies
+		companies = DumbStockClient.get_companies.split("}")
+		companies.each do |company|
+			companies_info = company.gsub('"','').gsub('{','').split(",").reject { |empty_string| empty_string.empty? || empty_string == "[" || empty_string == "]" }
+			cleaned_companies = companies_info.each{|datum| datum.gsub("[","")}
+			company_array = cleaned_companies.map{ |company_datum| company_datum.gsub("[","").split(":") }
+			company_hashes = company_array.map{ |pair| Hash[*pair] if pair.count > 1 }.compact
+			if !company_hashes.nil? && !(company_hashes == [])
+				company_data = Company.new()
+				company_data.symbol = company_hashes[0]["ticker"] 
+				company_data.name = company_hashes[1]["name"]
+				company_data.exchange = company_hashes[3]["exchange"]
+
+				company_data.save
+			end
+		end
+	end
 end
